132ee4cfd4eac9dabb7b1861f15372cc73a2794a
Netskar A <netskaram1994@gmail.com>
2017-08-15 20:07:40 -0600

    .

--- NLogExample/NLogExample/Properties/AssemblyInfo.cs
+++ NLogExample/NLogExample/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
 // para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
 // COM, establezca el atributo ComVisible en true en este tipo.
