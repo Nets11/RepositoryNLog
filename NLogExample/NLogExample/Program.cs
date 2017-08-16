3bf4cbd2e095f441ec33c0c37ce4967b70e2f14c
Netskar A <netskaram1994@gmail.com>
2017-08-15 20:06:27 -0600

    NlogExample

---
+++ NLogExample/NLogExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NLogExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = 0;
+                int result = 5 / num;
+
+            }
+            catch(DivideByZeroException ex)
+            {
+                // Logging Exception UsandoNLog
+                Logger logger = LogManager.GetLogger("databaseLogger");
+
+                logger.Error(ex, "Error");
+            }
+
+            // Reportar lo que se hizo con el Method Main
+            Console.WriteLine("Ahora pasaremos a la Exception Handler Logic");
+            Console.ReadLine();
+
+        }
+    }
+}
